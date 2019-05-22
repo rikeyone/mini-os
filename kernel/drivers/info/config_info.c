530a087589504cef8caadea9739ecf98cd70db33
rikeyone <xiehaocheng127@163.com>
2019-05-22 16:34:18 +0800

    minios: Add linux kernel driver samples.
    Change-Id: I7689e027ffc703db433c54cc9766cdf889401f2f
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

---
+++ kernel/drivers/info/config_info.c
@@ -0,0 +1,36 @@
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+#include "config_info.h"
+
+static int config_info_proc_show(struct seq_file *m, void *v)
+{
+	int num = ARRAY_SIZE(config);
+	int i;
+	for (i = 0; i < num; i++)
+		seq_printf(m, "%s\n", config[i]);
+	return 0;
+}
+
+static int config_info_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, config_info_proc_show, NULL);
+}
+
+static const struct file_operations config_info_proc_fops = {
+	.open		= config_info_proc_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int __init proc_config_info_init(void)
+{
+	proc_create("config_info", 0, NULL, &config_info_proc_fops);
+	return 0;
+}
+module_init(proc_config_info_init);
+
