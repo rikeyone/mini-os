12fac850650720fb1319f4e3ecb469bba6287bab
rikeyone <xiehaocheng127@163.com>
2019-12-30 15:32:33 +0800

    minios:Support libc porting for arm/arm64 platform.
    Change-Id: I61f3972c2af05eb0db4a8073d92ba6306c4b8225
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

--- scripts/build_libc.sh
+++ scripts/build_libc.sh
@@ -13,21 +13,40 @@ if [ x"${ROOTDIR}" = x ];then
     ROOTDIR=$(dirname ${SCRIPT_DIR})
 fi
 
+TARGET=$1
+if [ ${TARGET} = "arm64" ];then
 # extract aarch64 gcc toolchain
-if [ ! -e /opt/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/ ]; then
-	tar -xvf ${ROOTDIR}/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu.tar.xz -C ${ROOTDIR}/toolchain/
-	SRC=${ROOTDIR}/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu
-else
+if [ -e /opt/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/ ]; then
 	echo "aarch64-linux-gnu-gcc version 4.9 already installed to /opt!"
 	SRC=/opt/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu
+else
+	exit 1
 fi
 
-TARGET=${ROOTDIR}/rootfs/arm64/_install
+INSTALL=${ROOTDIR}/rootfs/arm64/_install
 
-mkdir -p ${TARGET}/lib
-mkdir -p ${TARGET}/usr/lib
-cp -af ${SRC}/aarch64-linux-gnu/lib64/*.so* ${TARGET}/lib
-cp -af ${SRC}/aarch64-linux-gnu/libc/lib/*.so* ${TARGET}/lib
-cp -af ${SRC}/aarch64-linux-gnu/libc/usr/lib/*.so* ${TARGET}/usr/lib
+mkdir -p ${INSTALL}/lib
+mkdir -p ${INSTALL}/usr/lib
+cp -af ${SRC}/aarch64-linux-gnu/lib64/*.so* ${INSTALL}/lib
+cp -af ${SRC}/aarch64-linux-gnu/libc/lib/*.so* ${INSTALL}/lib
+cp -af ${SRC}/aarch64-linux-gnu/libc/usr/lib/*.so* ${INSTALL}/usr/lib
+else
+# extract arm gcc toolchain
+if [ -e /opt/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabi/ ]; then
+	echo "arm-linux-gnueabi-gcc version 4.9 already installed to /opt!"
+	SRC=/opt/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabi
+else
+	exit 1
+fi
+
+INSTALL=${ROOTDIR}/rootfs/arm/_install
+
+mkdir -p ${INSTALL}/lib
+mkdir -p ${INSTALL}/usr/lib
+cp -af ${SRC}/arm-linux-gnueabi/lib/*.so* ${INSTALL}/lib
+cp -af ${SRC}/arm-linux-gnueabi/libc/lib/*.so* ${INSTALL}/lib
+cp -af ${SRC}/arm-linux-gnueabi/libc/usr/lib/*.so* ${INSTALL}/usr/lib
+
+fi
 
 echo "Build libc success!!!"
